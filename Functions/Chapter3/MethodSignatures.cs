cd161b83d0e9ed2a60cae5d1d62d936f7ab9e4f5
alereca <alejandrorecalde5@gmail.com>
2020-02-11 01:57:16 -0300

    Add lookup extension method ex 2 ch 3

--- Functions/Chapter3/MethodSignatures.cs
+++ Functions/Chapter3/MethodSignatures.cs
@@ -1,24 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using LaYumba.Functional;
 using static LaYumba.Functional.F;
 
 namespace Functions.Chapter3
 {
-    public class MethodSignatures
+    public static class MethodSignatures
     {
         public enum DayOfWeek { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
 
         public static Option<DayOfWeek> DayOfWeekParse(string day)
         {
-            return day switch {
+            return day switch
+            {
                 "Monday" => Some(DayOfWeek.Monday),
-                "Tuesday" =>  Some(DayOfWeek.Tuesday),
-                "Wednesday" =>  Some(DayOfWeek.Wednesday),
-                "Thursday" =>  Some(DayOfWeek.Thursday),
-                "Friday" =>  Some(DayOfWeek.Friday),
-                "Saturday" =>  Some(DayOfWeek.Saturday),
-                "Sunday" =>  Some(DayOfWeek.Sunday),
+                "Tuesday" => Some(DayOfWeek.Tuesday),
+                "Wednesday" => Some(DayOfWeek.Wednesday),
+                "Thursday" => Some(DayOfWeek.Thursday),
+                "Friday" => Some(DayOfWeek.Friday),
+                "Saturday" => Some(DayOfWeek.Saturday),
+                "Sunday" => Some(DayOfWeek.Sunday),
                 _ => None
             };
+        }
+
+        public static Option<T> Lookup<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+        {
+            foreach (T t in list)
+            {
+                if (predicate(t))
+                {
+                    return Some(t);
+                }
+            }
+
+            return None;
         }
     }
 }
--- Tests/Chapter3/MethodSignaturesTests.cs
+++ Tests/Chapter3/MethodSignaturesTests.cs
@@ -2,6 +2,9 @@ using LaYumba.Functional;
 using static LaYumba.Functional.F;
 using Xunit;
 using static Functions.Chapter3.MethodSignatures;
+using System.Collections.Generic;
+using Functions.Chapter3;
+using System.Collections;
 
 namespace Tests.Chapter3
 {
@@ -19,6 +22,27 @@ namespace Tests.Chapter3
             var dayResult = DayOfWeekParse(day);
             //Assert
             Assert.Equal(expected: expectedDay, actual: dayResult);
+        }
+
+        // 2 Write a Lookup function that will take an IEnumerable and a predicate, and
+        // return the first element in the IEnumerable that matches the predicate, or None
+        // if no matching element is found. Write its signature in arrow notation:
+
+        // bool isOdd(int i) => i % 2 == 1;
+        // new List<int>().Lookup(isOdd) // => None
+        // new List<int> { 1 }.Lookup(isOdd) // => Some(1)
+        // Arrow Notation = Lookup: (IEnumerable, T => bool) => T
+        [Fact]
+        public void Lookup_ShouldReturnSomeWhenThePredicateIsSatisfiedOrNoneWhenItsNot()
+        {
+            //Arrange
+            bool predicate(int i) => i % 2 == 1;
+            //Act
+            var someResult = new List<int> { 3, 2, 1, 4 }.Lookup(predicate);
+            var noneResult = new List<int> { }.Lookup(predicate);
+            //Assert
+            Assert.Equal(expected: Some(3), actual: someResult);
+            Assert.Equal(expected: None, actual: noneResult);
         }
     }
 }
