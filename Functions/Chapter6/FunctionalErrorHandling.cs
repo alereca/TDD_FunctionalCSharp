3de9a60e2cd8e9ed27f40f466c2bb13b341c926e
alereca <alejandrorecalde5@gmail.com>
2020-02-19 02:47:08 -0300

    Add option and either chainable bind methods ex 2 ch 6

--- Functions/Chapter6/FunctionalErrorHandling.cs
+++ Functions/Chapter6/FunctionalErrorHandling.cs
@@ -1,3 +1,4 @@
+using System;
 using LaYumba.Functional;
 using static LaYumba.Functional.F;
 
@@ -8,7 +9,17 @@ namespace Functions.Chapter6
         public static Option<R> ToOption<L, R>(this Either<L, R> either)
             => either.Match((l) => None, (r) => Some(r));
 
-        public static Either<L,R> ToEither<L,R>(this Option<R> opt, L left)
-            => opt.Match<Either<L,R>>(() => Left(left), (r) => Right(r));
+        public static Either<L, R> ToEither<L, R>(this Option<R> opt, L left)
+            => opt.Match<Either<L, R>>(() => Left(left), (r) => Right(r));
+
+        public static Option<R> Bind<T, L, R>(this Option<T> opt, Func<T, Either<L, R>> f)
+            => opt.Match(
+                () => None, 
+                (t) => f(t).ToOption());
+
+        public static Option<RR> Bind<L, R, RR>(this Either<L,R> either, Func<R,Option<RR>> f)
+            => either.Match(
+                (l) => None, 
+                (r) => f(r));
     }
 }
--- Tests/Chapter6/FunctionalErrorHandlingTests.cs
+++ Tests/Chapter6/FunctionalErrorHandlingTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using static LaYumba.Functional.F;
+using static Functions.Chapter3.MethodSignatures;
 using Functions.Chapter6;
 using LaYumba.Functional;
 
@@ -16,7 +17,7 @@ namespace Tests.Chapter6
         // ToEither: (Option<R>, L) => Either<L,R>
         [Theory]
         [ClassData(typeof(ToOptionTestData))]
-        public void ToOption_ShouldConvertAnEitherToAnOptionDisposingItsLeftValue(Either<string,string> either, Option<string> expected)
+        public void ToOption_ShouldConvertAnEitherToAnOptionDisposingItsLeftValue(Either<string, string> either, Option<string> expected)
         {
             //Act
             var opt = either.ToOption();
@@ -26,12 +27,49 @@ namespace Tests.Chapter6
 
         [Theory]
         [ClassData(typeof(ToEitherTestData))]
-        public void ToEither_ShouldCovertAnOptionToAnEitherTakingSomeLeftValueArgument(Option<string> opt, Either<string,string> expected)
+        public void ToEither_ShouldCovertAnOptionToAnEitherTakingSomeLeftValueArgument(Option<string> opt, Either<string, string> expected)
         {
             //Act
             var either = opt.ToEither("error");
             //Assert
             Assert.Equal(expected: expected, actual: either);
+        }
+
+        // 2. Take a workflow where 2 or more functions that return an `Option`
+        // are chained using `Bind`.
+
+        // Then change the first one of the functions to return an `Either`.
+
+        // This should cause compilation to fail. Since `Either` can be
+        // converted into an `Option` as we have done in the previous exercise,
+        // write extension overloads for `Bind`, so that
+        // functions returning `Either` and `Option` can be chained with `Bind`,
+        // yielding an `Option`.
+        [Fact]
+        public void OptionBind_ShouldBeChainableByFunctionsReturningEither()
+        {
+            // Note: type inference can't guess which type is the L in either<L,R>
+            //Act
+            var result =
+            Some("ale")
+            .Bind<string, string, string>(s => Right(s.Trim()))
+            .Bind(s => Some(s.Length));
+
+            //Assert
+            Assert.Equal(expected: 3, actual: result);
+        }
+
+        [Fact]
+        public void EitherBind_ShouldBeChainableByFunctionsReturningOption()
+        {
+            //Act
+            var result =
+            DayOfWeekParse("Friday")
+            .ToEither("Day not valid")
+            .Bind(d => Some(d.ToString()));
+
+            //Assert
+            Assert.Equal(expected: Some("Friday"), actual: result);
         }
     }
 }
