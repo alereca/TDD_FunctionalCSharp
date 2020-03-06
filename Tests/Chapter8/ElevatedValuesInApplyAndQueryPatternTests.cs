1684fcd3d00534bce07106fb5730ecf1ec07bf2e
alereca <alejandrorecalde5@gmail.com>
2020-03-06 05:08:54 -0300

    Improve test in apply and query pattern for exceptional ex 1 and 2 ch 8

--- Tests/Chapter8/ElevatedValuesInApplyAndQueryPatternTests.cs
+++ Tests/Chapter8/ElevatedValuesInApplyAndQueryPatternTests.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 using static LaYumba.Functional.F;
-using static Functions.Chapter8.ElevatedValuesInApplyAndQueryPattern;
+using Functions.Chapter8;
 using Functions.Chapter6;
 
 namespace Tests.Chapter8
@@ -9,35 +9,50 @@ namespace Tests.Chapter8
     public class ElevatedValuesInApplyAndQueryPatternTests
     {
         // Implement apply for either and exceptional  
-        [Fact]
-        public void ApplyForExceptional()
+        [Theory]
+        [InlineData(2, 1, true)]
+        [InlineData(666, 1, false)]
+        [InlineData(2, 666, false)]
+        public void ApplyForExceptional(int x, int y, bool success)
         {
             //Arrange
+            Func<int,LaYumba.Functional.Exceptional<int>> numOrDevil = (i) => i switch {
+                666 => new Exception("The number of the devil"),
+                _ => i
+            };
             Func<int, int, int> difference = (x, y) => x - y;
             //Act
             var result = Exceptional(difference)
-                .Apply(2)
-                .Apply(1);
+                .Apply(numOrDevil(x))
+                .Apply(numOrDevil(y));
 
             //Assert
-            Assert.Equal(expected: 1, actual: result);
+            Assert.Equal(expected: success, actual: result.Success);            
         }
 
 
         // Implement the query pattern for `Either` and `Exceptional`. 
         // Try to write down the signatures for `Select` and `SelectMany` without looking at any examples. 
         // For the implementation, just follow the types—if it type checks, it’s probably right!
-        [Fact]
-        public void QueryPatternForExceptional()
+        [Theory]
+        [InlineData(2, 1, 1, true)]
+        [InlineData(666, 1, 33, false)]
+        [InlineData(2, 666, 17, false)]
+        public void QueryPatternForExceptional(int x1, int y1, int z1, bool expected)
         {
+            //Arrange
+            Func<int,LaYumba.Functional.Exceptional<int>> numOrDevil = (i) => i switch {
+                666 => new Exception("The number of the devil"),
+                _ => i
+            };
             //Act
-            var result = 
-                from x in Exceptional(2)
-                from y in Exceptional(2)
-                from z in Exceptional(2)
-                select x*y*z;
+            var result =
+                from x in numOrDevil(x1)
+                from y in numOrDevil(y1)
+                from z in numOrDevil(z1)
+                select x * y * z;
             //Assert
-            Assert.Equal(expected: 8, actual: result);
+            Assert.Equal(expected: expected, actual: result.Success);
         }
 
         [Theory]
@@ -45,7 +60,7 @@ namespace Tests.Chapter8
         public void QueryPatternForEither(string xStr, string yStr, string zStr, LaYumba.Functional.Either<string, int> expected)
         {
             //Act
-            var result = 
+            var result =
                 from x in LaYumba.Functional.Int.Parse(xStr).ToEither("Parse operation failed")
                 from y in LaYumba.Functional.Int.Parse(yStr).ToEither("Parse operation failed")
                 from z in LaYumba.Functional.Int.Parse(zStr).ToEither("Parse operation failed")
@@ -54,8 +69,20 @@ namespace Tests.Chapter8
             Assert.Equal(expected: expected, actual: result);
         }
 
+        /*
         // Come up with a scenario in which various `Either`-returning operations are chained with `Bind`. 
         // (If you’re short of ideas, you can use the favorite-dish example from chapter 6.) 
         // Rewrite the code using a LINQ expression.
+        [Fact]
+        public void EitherReturningOperationsChainedByBind()
+        {
+            var result = LaYumba.Functional.Int.Parse("4")
+                .ToEither("Parse operation failed")
+                .Bind(i => (LaYumba.Functional.Either<string, int>)Right(i * 2))
+                .Bind(i => (LaYumba.Functional.Either<string, int>)Right(i - 2));
+
+            Assert.Equal(expected: 6, actual: result);
+        }
+        */
     }
 }
